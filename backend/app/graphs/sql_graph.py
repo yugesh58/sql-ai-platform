95958dab1987eb92afbc131d63e06e176f0371e8
Yugesh <yugesh@Yugeshs-MacBook-Air.local>
2026-05-31 17:46:18 +0530

    Added conditional routing and retry workflow

--- backend/app/graphs/sql_graph.py
+++ backend/app/graphs/sql_graph.py
@@ -7,6 +7,7 @@ class SQLState(TypedDict):
     question:str
     sql_query:str
     validation_status:str
+    retry_count:int
     result:list
 
 def sql_generation_node(state:SQLState):
@@ -23,6 +24,20 @@ def validate_sql_node(state:SQLState):
         return{"validation_status":"invalid"}
     else:
         return{"validation_status":"valid"}
+    
+def regenrate_sql_node(state:SQLState):
+    question=state["question"]
+    sql_query=generate_sql(question)
+
+    retry_count=state.get("retry_count",0)+1
+    return{"sql_query":sql_query,"retry_count":retry_count}
+
+def validation_router(state:SQLState):
+    if state["validation_status"]=="valid":
+        return "execute_sql"
+    if state.get("retry_count",0) < 2:
+        return "retry_sql"
+    return "end"
 
 def query_executor_node(state:SQLState):
     sql_query=state['sql_query']
@@ -32,14 +47,28 @@ def query_executor_node(state:SQLState):
 
 
 graph_builder=StateGraph(SQLState)
-
+#nodes
 graph_builder.add_node("generate_sql",sql_generation_node)
+
 graph_builder.add_node("validate_sql",validate_sql_node)
+
 graph_builder.add_node("execute_sql",query_executor_node)
+graph_builder.add_node("retry_sql",regenrate_sql_node)
+#edges
+graph_builder.add_conditional_edges(
+    "validate_sql",
+    validation_router,
+    {
+        "execute_sql": "execute_sql",
+        "retry_sql":"retry_sql",
+        "end": END
+    }
+)
+
 
 graph_builder.add_edge(START,"generate_sql")
 graph_builder.add_edge("generate_sql","validate_sql")
-graph_builder.add_edge("validate_sql","execute_sql")
+graph_builder.add_edge("retry_sql","validate_sql")
 graph_builder.add_edge("execute_sql",END)
 
 sql_graph=graph_builder.compile()
--- backend/app/graphs/test_graph.py
+++ backend/app/graphs/test_graph.py
@@ -2,7 +2,8 @@ from app.graphs.sql_graph import sql_graph
 
 result = sql_graph.invoke(
     {
-        "question": "Show all employees"
+        "question": "Show all clients",
+        "retry_count": 0
     }
 )
 
